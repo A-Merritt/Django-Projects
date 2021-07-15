bef16b7558a2055976387229f4c61c9a7b90703f
Austin Merritt <merritaj@gmail.com>
2021-07-15 00:18:37 -0500

    Add files via upload
    Created a Login and Registration form while learning how to encrypt/hash a password for a User using Bcrypt

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restful_tv.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loginAndregistration.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
